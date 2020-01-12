9ff65afdde9291b835c87a80e36c12b13670db62
alex-diachenko <dyachenko664@gmail.com>
2020-01-12 18:04:33 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,66 @@
+  // owl carousel main
+
+  $('.owl_first').owlCarousel({
+    loop:true,
+    autoplay:true, 
+    autoplayTimeout:4500,
+    autoplayHoverPause:true,
+    margin:0,
+    nav:true,
+    navText : ["",""],
+    dots: true,
+    dots: 3,
+    lazyLoad: true,
+    responsive:{
+    0:{
+        items:1
+        },
+    600:{
+          items:1
+        },
+    800:{
+        items:1
+        },
+    1240: {
+        items: 1
+    }
+    }
+  });    
+
+  // second carousel
+
+  $('.owl_second').owlCarousel({
+    loop:true,
+    autoplay:true, 
+    autoplayTimeout:4500,
+    autoplayHoverPause:true,
+    margin:0,
+    nav:true,
+    navText : ["",""],
+    dots: false,
+    lazyLoad: true,
+    responsive:{
+    0:{
+        items:1
+        },
+    888:{
+        items: 1
+        },
+    1300: {
+        items: 2
+    },
+    1320: {
+      items: 3
+    },
+    }
+  }); 
+
+// select
+
+$(document).ready(function () {
+  $('#tours').chosen({
+    disable_search: true,
+    placeholder_text_single: "Страна тура",
+  });
+});
+
